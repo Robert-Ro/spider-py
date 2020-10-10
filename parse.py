c3aa4f1511f57be5ed210f18933a331cbb5db3d9
a20168 <a20168@auteltech.net>
2020-10-10 20:35:27 +0800

    feat: 处理依赖等

--- parse.py
+++ parse.py
@@ -1,5 +1,11 @@
 from lxml import etree
+import redis
 
 html = etree.parse('./data/index.html', etree.HTMLParser())
 result = html.xpath('//*')
-print(result)
+r = redis.Redis(host='localhost', port=6379, db=0)
+res = r.get('test')
+print(res)
+# TODO
+# 1. get all link to save redis
+# 2. fork child-process or use thread to crawl
